104834ab745e6c793fdd40c6360756da485125ce
Yue Lin Ho <b8732003@student.nsysu.edu.tw>
2017-07-06 03:03:38 +0800

    Pass combobox as reference instead of pointer
    Signed-off-by: Yue Lin Ho <b8732003@student.nsysu.edu.tw>
    Signed-off-by: Sven Strickroth <email@cs-ware.de>

--- src/TortoiseProc/RebaseDlg.cpp
+++ src/TortoiseProc/RebaseDlg.cpp
@@ -2478,7 +2478,7 @@ void CRebaseDlg::OnBnClickedButtonReverse()
 
 void CRebaseDlg::OnBnClickedButtonBrowse()
 {
-	if(CBrowseRefsDlg::PickRefForCombo(&m_UpstreamCtrl))
+	if (CBrowseRefsDlg::PickRefForCombo(m_UpstreamCtrl))
 		OnCbnSelchangeUpstream();
 }
 
